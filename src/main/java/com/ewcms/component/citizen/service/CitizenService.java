f0eb11a7698caaf8acbb25b49c29eee95ead58ac
wuzhijun <wu_zhijun@msn.com>
2012-08-22 00:35:33 +0800

    新建人员信息 代码说明文档

---
+++ src/main/java/com/ewcms/component/citizen/service/CitizenService.java
@@ -0,0 +1,45 @@
+package com.ewcms.component.citizen.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ewcms.component.online.dao.CitizenDAO;
+import com.ewcms.component.online.vo.Citizen;
+
+/**
+ * @author wuzhijun
+ */
+@Service
+public class CitizenService {
+    @Autowired
+    private CitizenDAO citizenDAO;
+
+    public void setCitizenDAO(CitizenDAO dao) {
+        this.citizenDAO = dao;
+    }
+    
+    public List<Citizen> getCitizenAll() {
+        return citizenDAO.getAll();
+    }
+    
+    public String mainJSON(){
+        List<Citizen> citizens = getCitizenAll();
+        if(citizens.isEmpty()){
+            return "[]";
+        }
+        
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        for(Citizen citizen : citizens){
+            builder.append("{").append("\"id\":\"").append(citizen.getId()).append("\",");
+            builder.append("\"name\":\"").append(citizen.getName()).append("\"},");
+        }
+        builder.deleteCharAt(builder.length() -1);
+        builder.append("]");
+
+        return builder.toString();
+    }
+
+}
